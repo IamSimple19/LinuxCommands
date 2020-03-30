39131767650be62f0d93da33f5c85e9a28b36f1d
IamSimple19 <keepitsimple1331@gmail.com>
2020-03-30 16:06:20 +0530

    Added if-else and export use

---
+++ ss3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Use of conditions
+echo "Enter your name please:"
+read name
+if [ $name == 'Jagtar' ] || [ ${name^^} == "JAGTAR" ] #Command to convert lcase and ucase on the fly
+then 
+	echo "Hey man!!! Today is `date`" #tilde quotes works as command not a string
+elif [ $name == "Ravi" ]
+	then
+		echo "Welcome Ravi!!"
+else
+	echo "Oh! you are not that one"
+fi
+
+#Bonus
+#How to use export command in shell
+
+name (){   #Define a function
+
+	echo "Hey!! man How are you doing?"
+}
+
+name  #This is how we call a function just simply name no  use of ()
+
+#export variable
+Age=26  #Define var
+export Age #Export that var in env library
+echo $Age #print that env var 
