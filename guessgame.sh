9d0c472efa85802a9b81905f20713e514dc557dc
IamSimple19 <keepitsimple1331@gmail.com>
2020-04-06 14:30:02 +0530

    added

--- guessgame.sh
+++ guessgame.sh
@@ -4,7 +4,7 @@ number=$(( $RANDOM % 10 ))   #This assures the number is less than 100
 
 guess=     #defined empty var
 
-until [[ $guess -eq $number ]]; do #Loop 
+until [[ $guess -eq $number ]]; do #Loop untill
 	read -p "Enter your guess:" guess
 	if [[ $guess -eq $IFS ]]; then
 		echo "Please enter a number"
