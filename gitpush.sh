8d112e533169b96b0c20ebed9fd211905a84e1b9
IamSimple19 <keepitsimple1331@gmail.com>
2020-04-06 13:58:01 +0530

    m

--- gitpush.sh
+++ gitpush.sh
@@ -3,6 +3,15 @@
 newfile=$( ls -t | head -n 2 | grep .sh ) #this will fetch the latest .sh file
 
 echo "This file is latest $newfile"
+gitignorefile="`pwd`/.gitignore"
+if [[ ! -e $gitignorefile  ]]; then
+	echo "****Creating gitignore file*******"
+	touch .gitignore
+	echo "gitpush.sh" >> .gitignore
+else
+	echo "*******checking gitignore file*******"
+	echo "**********File already exists********"
+fi
 
 read -p "Do you want to commit this file:" answer
 if [[ $answer = [Yy] ]]; then
