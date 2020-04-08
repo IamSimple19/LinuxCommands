a4899e35bf13010c277e4d068d6351b0b1a028ff
IamSimple19 <keepitsimple1331@gmail.com>
2020-04-08 13:22:33 +0530

    how-to-run-script-at-backend

---
+++ backendscript.sh
@@ -0,0 +1,8 @@
+#How to put scripts at backend
+
+myscript & ----- this will put script at backend but when you close terminal it will end
+nohup myscript & ----- this will run script at backend even you close the terminal
+
+#how to give low priority to scipt while running it
+
+nice myscript
